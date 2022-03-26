81039ec2cad27fb9e355ffc86c3f42e628e9b4f4
Romain FREZIER <romain.frezier@etu.umontpellier.fr>
2022-03-26 10:07:09 +0100

    trying to resolve host problems

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@
 
 // Use it for heroku host
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
         ssl: {
             require: true,
